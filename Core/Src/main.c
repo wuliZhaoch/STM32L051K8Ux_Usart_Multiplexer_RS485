19411a17b1a39c8af58886f791a309c5275424dd
Zhao_ch <Zhao_ch@163.com>
2021-04-19 19:01:11 +0800

    Serial port to RS485 send OK (手动切换RS485_DERE引脚)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,7 +2,7 @@
 
 uint32_t main_loop = 0;
 
-uint8_t RS485_Send_buffer[5] = {0};
+uint8_t RS485_Send_buffer[5] = {0x12, 0x34, 0x56, 0x12, 0x55};
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
@@ -30,6 +30,11 @@ int main(void)
     MX_TIM6_Init();
 
     SGM4581_Address_set(Select_Address_x7);
+
+    HAL_GPIO_WritePin(GPIOA, RS485_REDE_Pin, GPIO_PIN_SET);
+    HAL_UART_Transmit(&huart1, RS485_Send_buffer, 5, 1000);
+    HAL_Delay(500);
+    HAL_GPIO_WritePin(GPIOA, RS485_REDE_Pin, GPIO_PIN_RESET);
     while (1)
     {
 
