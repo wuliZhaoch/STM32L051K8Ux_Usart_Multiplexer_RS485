a23dfad02e46d5934f4be9139e3071a22dc1572a
Zhao_ch <Zhao_ch@163.com>
2021-04-19 12:58:50 +0800

    Add System Config Files

---
+++ Core/Inc/includes.h
@@ -0,0 +1,31 @@
+/*
+ * includes.h
+ *
+ *  Created on: 2021-03-28
+ *      Author: wuliZhao_ch
+ */
+ 
+#ifndef INCLUDES_H_
+#define INCLUDES_H_
+
+#include "common.h"
+
+#include "system.h"
+
+#include "gpio.h"
+
+#include "usart.h"
+
+#include "dma.h"
+
+#include "chip_id.h"
+
+#include "chip_eeprom.h"
+
+#include "i2c.h"
+
+#include "bmx160.h"
+
+#include "delay.h"
+
+#endif /* INCLUDES_H_ */
---
+++ Core/Inc/system.h
@@ -0,0 +1,23 @@
+/*
+ * system.h
+ *
+ *  Created on: 2021.3.29
+ *      Author: Wulizhaoch
+ *
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+#include "common.h"
+
+
+void RCC_CLK_Enable(void);
+
+void SystemClock_Config(void);
+
+void Error_Handler(void);
+
+
+
+#endif /* SYSTEM_H_ */
