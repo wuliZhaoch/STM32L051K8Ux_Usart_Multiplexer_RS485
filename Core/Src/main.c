9a13c98bcdb4a0f73d3bb32edc25990cea79b077
Zhao_ch <Zhao_ch@163.com>
2021-04-21 19:22:55 +0800

    Data set package 数据组包(简单组包还待分析处理)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -9,6 +9,8 @@ uint8_t GY301_CMD = 0XAA;
 uint8_t GY53L1_Buf_CMD[3] = {0XA5, 0X15, 0XBA};
 uint8_t ODOA_Buff[2] = {0x0d,0x0a};
 
+uint8_t rs485_buf[23] = {0};
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
     if (htim == &htim6)
@@ -38,8 +40,6 @@ int main(void)
     MX_I2C1_Init();         // System IIC Initialize
     MX_TIM6_Init();         // System Timer Initialize (1S)
 
-//    SGM4581_Address_set(Select_Address_x7);     // Set up channel 7 to receive data
-
 
 
     while (1)
@@ -60,16 +60,31 @@ int main(void)
         HAL_Delay(100);
 
 
+        for (uint8_t i = 0; i < GY53L1_RECEIVE_LEN; i++)
+        {
+            rs485_buf[i] = GY53L1_revDef.GY53L1_pData[i];
+        }
+        for (uint8_t i = 0; i < SR20_RECEIVE_LEN; i++)
+        {
+            rs485_buf[i + GY53L1_RECEIVE_LEN] = JSN_SR20_revDef.JSN_SR20_pData[i];
+        }
+        for (uint8_t i = 0; i < GY301_RECEIVE_LEN; i++)
+        {
+            rs485_buf[i + GY53L1_RECEIVE_LEN + SR20_RECEIVE_LEN] = GY301_revDef.GY301_pData[i];
+        }
+
         RS485_TRANSMIT_MODE;
         HAL_Delay(10);
-        HAL_UART_Transmit(&huart1, GY53L1_revDef.GY53L1_pData, GY53L1_RECEIVE_LEN, 200);
+        HAL_UART_Transmit(&huart1, rs485_buf, 23, 200);
         HAL_Delay(100);
-
-        HAL_UART_Transmit(&huart1, JSN_SR20_revDef.JSN_SR20_pData, SR20_RECEIVE_LEN, 200);
-        HAL_Delay(100);
-
-        HAL_UART_Transmit(&huart1, GY301_revDef.GY301_pData, GY301_RECEIVE_LEN, 200);
-        HAL_Delay(100);
+//        HAL_UART_Transmit(&huart1, GY53L1_revDef.GY53L1_pData, GY53L1_RECEIVE_LEN, 200);
+//        HAL_Delay(100);
+//
+//        HAL_UART_Transmit(&huart1, JSN_SR20_revDef.JSN_SR20_pData, SR20_RECEIVE_LEN, 200);
+//        HAL_Delay(100);
+//
+//        HAL_UART_Transmit(&huart1, GY301_revDef.GY301_pData, GY301_RECEIVE_LEN, 200);
+//        HAL_Delay(100);
         RS485_RECEIVE_MODE;
 
         HAL_Delay(2000);
