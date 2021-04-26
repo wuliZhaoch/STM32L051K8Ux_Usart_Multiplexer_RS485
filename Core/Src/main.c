5cb9d1afdb8dd4a793bd11da2c3b35e318a14cb2
Zhao_ch <Zhao_ch@163.com>
2021-04-26 15:19:03 +0800

    打印八个通道的数据

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -3,7 +3,17 @@
 #define ADDRESS_DELAY   100
 #define CMD_DELAY       200
 
+#define DATA_TOTAL_LEN  92
+
+#define RS485_ADDRESS_CODE          0x01
+#define RS485_FUNCTION_CODE         0x06
+
+#define RS485_ADDRESS_CODE_LEN      1
+#define RS485_FUNCTION_CODE_LEN     1
+#define RS485_CRC_LEN               2
+
 uint32_t main_loop = 0;
+uint8_t rs485_time = 0;
 
 uint8_t RS485_Send_buffer[5] = {0x12, 0x55, 0xff, 0x12, 0x55};
 
@@ -13,21 +23,121 @@ uint8_t GY53L1_Buf_CMD[3] = {0xA5, 0x15, 0xBA};     // GY53L1 Read Command
 uint8_t GY56_Buf_CMD[4] = {0xA5, 0x65, 0x02, 0x0C}; // GY56 Read Command
 
 
+uint8_t rs485_Send_buf[DATA_TOTAL_LEN] = {0};
 
-uint8_t ODOA_Buff[2] = {0x0d,0x0a};
-
-uint8_t rs485_buf[23] = {0};
+void Scan_Sensor_config(uint8_t device_channel);
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
     if (htim == &htim6)
     {
 //        HAL_GPIO_TogglePin(SYSTEM_LED_GPIO_Port, SYSTEM_LED_Pin);
-
     }
 }
 
 
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+    HAL_Init();
+
+    /* System Clock Config */
+    SystemClock_Config();
+
+    /* System Delay Initialize */
+    HAL_Delay_init();
+    MX_GPIO_Init();         // System GPIO Initialize
+    MX_DMA_Init();          // System DMA Initialize
+    MX_USART1_UART_Init();  // System Usart1 Initialize (RS485)
+    MX_USART2_UART_Init();  // System Usart2 Initialize
+    MX_I2C1_Init();         // System IIC Initialize
+    MX_TIM6_Init();         // System Timer Initialize (1S)
+
+    PackBuffer.counter_time = 0;
+    Scan_Sensor_config(Select_Address_x0);
+    Scan_Sensor_config(Select_Address_x1);
+    Scan_Sensor_config(Select_Address_x2);
+    Scan_Sensor_config(Select_Address_x3);
+    Scan_Sensor_config(Select_Address_x4);
+    Scan_Sensor_config(Select_Address_x5);
+    Scan_Sensor_config(Select_Address_x6);
+    Scan_Sensor_config(Select_Address_x7);
+
+    memset(&USART2_ReceiveDef.Receive_pData, 0, sizeof(USART2_ReceiveDef.Receive_pData));
+    memset(&PackBuffer.channel_buffer, 0, sizeof(PackBuffer.channel_buffer));
+
+    memset(&JSN_SR20_revDef.JSN_SR20_pData, 0, sizeof(JSN_SR20_revDef.JSN_SR20_pData));
+    memset(&GY301_revDef.GY301_pData, 0, sizeof(GY301_revDef.GY301_pData));
+    memset(&GY53L1_revDef.GY53L1_pData, 0, sizeof(GY53L1_revDef.GY53L1_pData));
+    memset(&GY56_revDef.GY56_pData, 0, sizeof(GY56_revDef.GY56_pData));
+
+    System_LED_Blink(20, 50);
+    while (1)
+    {
+        PackBuffer.counter_time = 0;
+        memset(&PackBuffer.channel_buffer, 0, sizeof(PackBuffer.channel_buffer));
+
+        HAL_Delay_ms(100);
+        SGM4581_Address_set(Select_Address_x0);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd0, SenserLEN.channel_cmd0_len, 200);
+        HAL_Delay_ms(100);
+
+        SGM4581_Address_set(Select_Address_x1);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd1, SenserLEN.channel_cmd1_len, 200);
+        HAL_Delay_ms(100);
+
+        SGM4581_Address_set(Select_Address_x2);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd2, SenserLEN.channel_cmd2_len, 200);
+        HAL_Delay_ms(100);
+
+        SGM4581_Address_set(Select_Address_x3);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd3, SenserLEN.channel_cmd3_len, 200);
+        HAL_Delay_ms(100);
+
+        SGM4581_Address_set(Select_Address_x4);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd4, SenserLEN.channel_cmd4_len, 200);
+        HAL_Delay_ms(100);
+
+        SGM4581_Address_set(Select_Address_x5);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd5, SenserLEN.channel_cmd5_len, 200);
+        HAL_Delay_ms(100);
+
+        SGM4581_Address_set(Select_Address_x6);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd6, SenserLEN.channel_cmd6_len, 200);
+        HAL_Delay_ms(100);
+
+        SGM4581_Address_set(Select_Address_x7);
+        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd7, SenserLEN.channel_cmd7_len, 200);
+        HAL_Delay_ms(100);
+
+        for (uint8_t i = 0; i < CHANNEL_NUMBER; i++)
+        {
+            for (uint8_t j = 0; j < PACKAGE_BUFFER_LEN; j++)
+            {
+                rs485_Send_buf[rs485_time] = PackBuffer.channel_buffer[i][j];
+                rs485_time++;
+            }
+        }
+
+        rs485_time = 0;
+
+        RS485_TRANSMIT_MODE;
+        HAL_Delay(10);
+        HAL_UART_Transmit(&huart1, rs485_Send_buf, DATA_TOTAL_LEN, 100);
+        RS485_RECEIVE_MODE;
+
+    }
+
+}
+
+/**
+  * @brief  Power on Scan Sensor Device.
+  * @retval None.
+  */
 void Scan_Sensor_config(uint8_t device_channel)
 {
     switch(device_channel)
@@ -354,94 +464,4 @@ void Scan_Sensor_config(uint8_t device_channel)
             break;
     }
 }
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-
-    HAL_Init();
-
-    /* System Clock Config */
-    SystemClock_Config();
-
-    /* System Delay Initialize */
-    HAL_Delay_init();
-    MX_GPIO_Init();         // System GPIO Initialize
-    MX_DMA_Init();          // System DMA Initialize
-    MX_USART1_UART_Init();  // System Usart1 Initialize (RS485)
-    MX_USART2_UART_Init();  // System Usart2 Initialize
-    MX_I2C1_Init();         // System IIC Initialize
-    MX_TIM6_Init();         // System Timer Initialize (1S)
-
-    PackBuffer.counter_time = 0;
-    Scan_Sensor_config(Select_Address_x0);
-    Scan_Sensor_config(Select_Address_x1);
-    Scan_Sensor_config(Select_Address_x2);
-    Scan_Sensor_config(Select_Address_x3);
-    Scan_Sensor_config(Select_Address_x4);
-    Scan_Sensor_config(Select_Address_x5);
-    Scan_Sensor_config(Select_Address_x6);
-    Scan_Sensor_config(Select_Address_x7);
-
-    memset(&USART2_ReceiveDef.Receive_pData, 0, sizeof(USART2_ReceiveDef.Receive_pData));
-    memset(&PackBuffer.channel_buffer, 0, sizeof(PackBuffer.channel_buffer));
-
-    memset(&JSN_SR20_revDef.JSN_SR20_pData, 0, sizeof(JSN_SR20_revDef.JSN_SR20_pData));
-    memset(&GY301_revDef.GY301_pData, 0, sizeof(GY301_revDef.GY301_pData));
-    memset(&GY53L1_revDef.GY53L1_pData, 0, sizeof(GY53L1_revDef.GY53L1_pData));
-    memset(&GY56_revDef.GY56_pData, 0, sizeof(GY56_revDef.GY56_pData));
-
-    System_LED_Blink(20, 50);
-    while (1)
-    {
-        PackBuffer.counter_time = 0;
-        memset(&PackBuffer.channel_buffer, 0, sizeof(PackBuffer.channel_buffer));
-
-        HAL_Delay_ms(100);
-        SGM4581_Address_set(Select_Address_x0);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd0, SenserLEN.channel_cmd0_len, 200);
-        HAL_Delay_ms(100);
-
-        SGM4581_Address_set(Select_Address_x1);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd1, SenserLEN.channel_cmd1_len, 200);
-        HAL_Delay_ms(100);
-
-        SGM4581_Address_set(Select_Address_x2);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd2, SenserLEN.channel_cmd2_len, 200);
-        HAL_Delay_ms(100);
-
-        SGM4581_Address_set(Select_Address_x3);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd3, SenserLEN.channel_cmd3_len, 200);
-        HAL_Delay_ms(100);
-
-        SGM4581_Address_set(Select_Address_x4);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd4, SenserLEN.channel_cmd4_len, 200);
-        HAL_Delay_ms(100);
-
-        SGM4581_Address_set(Select_Address_x5);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd5, SenserLEN.channel_cmd5_len, 200);
-        HAL_Delay_ms(100);
-
-        SGM4581_Address_set(Select_Address_x6);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd6, SenserLEN.channel_cmd6_len, 200);
-        HAL_Delay_ms(100);
-
-        SGM4581_Address_set(Select_Address_x7);
-        HAL_UART_Transmit(&huart2, SenserCMD.channel_cmd7, SenserLEN.channel_cmd7_len, 200);
-        HAL_Delay_ms(100);
-
-
-        RS485_TRANSMIT_MODE;
-        HAL_Delay(10);
-        HAL_UART_Transmit(&huart1, rs485_buf, 23, 200);
-        RS485_RECEIVE_MODE;
-
-    }
-
-}
-
-
 
